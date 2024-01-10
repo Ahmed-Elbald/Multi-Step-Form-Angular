ba2ccc3fb019c919e368c77e60483b1b5111ce39
Ahmed El Bald <ahmedsaad112004cr@gmail.com>
2024-01-10 15:09:03 +0200

    Separated confirmation message in its own component

---
+++ src/app/purchase/ui/confirmation-msg/confirmation-msg.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'form-confirmation-msg',
+  standalone: true,
+  imports: [],
+  templateUrl: './confirmation-msg.component.html',
+  styleUrl: './confirmation-msg.component.scss'
+})
+export class ConfirmationMsgComponent {
+
+}

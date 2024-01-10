0cd5ac13e152b31ef181e8a9077e7ddfef568d37
Ahmed El Bald <ahmedsaad112004cr@gmail.com>
2024-01-10 15:11:01 +0200

    Changed add-ons data to be obtained from parent component

--- src/app/purchase/ui/add-ons/add-ons.component.ts
+++ src/app/purchase/ui/add-ons/add-ons.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { LetDirective } from '@ngrx/component';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { AsyncPipe } from '@angular/common';
 
 import { CardDirective } from '../../utils/directives/card.directive';
 import { LinkPipe } from '../../utils/pipes/link.pipe';
@@ -8,8 +9,7 @@ import { DurationPipe } from '../../utils/pipes/duration.pipe';
 import { PlanDuration } from '../../utils/models/state.model';
 import { Plan } from '../../utils/models/plan.model';
 import { ItemPriceComponent } from '../item-price/item-price.component';
-import { FormStore } from '../../data-access/form.store';
-import { AsyncPipe } from '@angular/common';
+import { AddOn } from '../../utils/models/add-on.model';
 
 @Component({
   selector: 'form-add-ons',
@@ -41,12 +41,12 @@ export class AddOnsComponent implements ControlValueAccessor {
   // Input variables
   @Input({ required: true }) public selectedPlan!: Plan | undefined;
   @Input({ required: true }) public duration!: PlanDuration;
+  @Input({ required: true }) addOns!: AddOn[];
 
   // Public fields
-  public addOns = inject(FormStore).addOns$;
   public includedAddOns: string[] = []
 
-  public onChange(includedAddOns: string[]) { }
+  public onChange(_includedAddOns: string[]) { }
   public onTouched() { }
 
   // Public methods
